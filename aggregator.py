69e493895a6fae34e7fead0abb6c941ac59c60a0
Kellen Fox <kellen@cablespeed.com>
2015-08-07 13:59:36 -0700

    fix path

--- aggregator.py
+++ aggregator.py
@@ -8,7 +8,7 @@ from zmq.devices.basedevice import ProcessDevice
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 log = logging.getLogger('logger')
 
-LOG_FILE_PATH = "./log"
+LOG_FILE_PATH = "/var/log/logagg"
 HWM = 50
 
 class LogWorker(object):
