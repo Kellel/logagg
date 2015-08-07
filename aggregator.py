e456c8770d4b951fd2574960515ab3b4349e03fa
Kellen Fox <kellen@cablespeed.com>
2015-08-07 10:27:53 -0700

    created log aggregator

---
+++ aggregator.py
@@ -0,0 +1,63 @@
+import sys
+import os
+
+import logging
+import zmq
+from zmq.devices.basedevice import ProcessDevice
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+log = logging.getLogger('logger')
+
+LOG_FILE_PATH = "./tmp"
+HWM = 50
+
+class LogWorker(object):
+    def __init__(self):
+        log.debug("Checking for LOG_FILE_PATH at %s", LOG_FILE_PATH)
+        if not os.path.exists(LOG_FILE_PATH):
+            log.debug("Creating LOG_FILE_PATH at %s", LOG_FILE_PATH)
+            os.makedirs(LOG_FILE_PATH)
+        self.files = {}
+
+    def run(self):
+        context = zmq.Context()
+
+        receiver = context.socket(zmq.PULL)
+        receiver.set_hwm(HWM)
+        receiver.bind("tcp://127.0.0.1:7770")
+
+        sender = context.socket(zmq.PUSH)
+        sender.set_hwm(HWM)
+        sender.connect("tcp://127.0.0.1:7777")
+
+        while True:
+            work = receiver.recv_json()
+            log.debug("WORK: %s", str(work))
+
+            application = work['app']
+            log.info("JOB [FOR: {}] {}".format(application, work['msg']))
+
+            if not application in self.files:
+                log.info("OPENING: %s", application)
+                self.files[application] = open(os.path.join(LOG_FILE_PATH, application), 'a')
+
+            log.debug("Writing line to file")
+            self.files[application].write(work['msg'] + "\n")
+
+            log.debug("Relaying MSG")
+            sender.send_json(work)
+            log.debug("JOB Complete.")
+
+    def cleanup(self):
+        log.debug("Closing %d log files", len(self.files))
+        for item in self.files.values():
+            item.close()
+
+if __name__ == "__main__":
+    logger = LogWorker()
+    try:
+        logger.run()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        logger.cleanup()
